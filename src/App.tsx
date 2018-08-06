a24e0ff2edb5acb6f05d677c90624e821ff3882a
cscasalme <cscasalme@gmail.com>
2018-08-05 22:01:11 -0700

    added helper file. added geolocation request to get coordinates added locationIQ api request to reverse geocode

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,80 @@
 import * as React from 'react';
 import './App.css';
+import { getStateInitials } from "./Helper"
 
 import logo from './logo.svg';
 
-class App extends React.Component {
+interface IAppState {
+  state: string,
+  locationStatus: string,
+};
+
+class App extends React.Component<any, IAppState> {
+
+  constructor(props:any) {
+    super(props);
+
+    this.state = {
+      locationStatus: "notFound",
+      state: "None",
+    };
+
+    this.getLocation = this.getLocation.bind(this);
+    this.success = this.success.bind(this);
+    this.error = this.error.bind(this);
+  }
+
+  public getLocation(data: any) {
+    if (data.address.country_code === "us") {
+      const initials: string = getStateInitials(data.address.state);
+      if (initials !== "None") {
+        this.state = {
+          locationStatus: "found",
+          state: initials,
+        };
+        alert(initials);
+      }
+    }
+  }
+
+  public success(position: any) {
+
+    const accessToken = "3cdf4045cb6490";
+    const latitudeString = String(position.coords.latitude);
+    const longitudeString = String(position.coords.longitude);
+    const reverseGeocode: string = "https://us1.locationiq.com/v1/reverse.php?key=" + accessToken + "&lat="
+                                  + latitudeString + "&lon=" + longitudeString + "&format=json"
+
+    alert(reverseGeocode);
+    fetch(reverseGeocode)
+      .then(response => response.json())
+      .then(data => this.getLocation(data));
+  }
+
+  public error(errorMessage: any) {
+    // Error faced when loading location
+    alert('An error has occured while retrieving location');
+    this.setState({
+      locationStatus: "notFound",
+      state: "None",
+    });
+  }
+
+  public componentDidMount() {
+    if ("geolocation" in navigator) {
+      /* geolocation is available */
+      alert("available");
+      navigator.geolocation.getCurrentPosition(this.success, this.error);
+    } else {
+      /* geolocation IS NOT available */
+      alert("Sorry, Congress does not work on this browser. :(");
+      this.setState({
+        locationStatus: "notFound",
+        state: "None",
+      });
+    }
+  }
+
   public render() {
     return (
       <div className="App">
